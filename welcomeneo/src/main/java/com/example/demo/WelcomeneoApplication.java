0f85bc5d2b44dbf14ff2afa34b67426a506e8963
Nithish-devx <125951185+Nithish-devx@users.noreply.github.com>
2023-02-23 12:15:38 +0530

    Add files via upload

---
+++ welcomeneo/src/main/java/com/example/demo/WelcomeneoApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WelcomeneoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WelcomeneoApplication.class, args);
+	}
+
+}
